8ed0846c7493cb5a763346f3c7d48a2bf5385416
Green <2228680344@qq.com>
2017-12-03 20:37:12 +0800

    Update mailTo.js

--- mailTo.js
+++ mailTo.js
@@ -3,14 +3,13 @@
 const nodeMailer = require('nodemailer');
 
 var transporter = nodeMailer.createTransport({
-              service: 'QQ',
-	          //host: "smtp.qq.com", // 主机
-			  secure: true, // 使用 SSL
-			  secureConnection: true, // 使用 SSL
-			  port: 25, // SMTP 端口
-			  auth: {
-	          user: '',
-	          pass: ''
+             	  service: 'QQ',
+		  secure: true, // 使用 SSL
+		  secureConnection: true, // 使用 SSL
+		  port: 25, // SMTP 端口
+		  auth: {
+	          user: '',//这是你的账号
+	          pass: ''//这是邮箱开启stmp服务的授权号
     		  }
 
 	     }),
@@ -18,7 +17,7 @@ var transporter = nodeMailer.createTransport({
 
     mailOptions = {
 			  from: "2228680344<2228680344@qq.com>", // 发件地址
-			  to: "1878693031@qq.com", // 收件列表
+			  to: "", // 收件列表
 			  subject: "Hello world", // 标题
 			
 			  html: "<b>thanks a for visiting!</b> 世界，你好！" // html 内容
